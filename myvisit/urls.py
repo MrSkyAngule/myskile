af95f5d29e43f55ebfac3b13245b349a12f07a5b
MrSkyAngule <garhok124@gmail.com>
2026-06-24 15:13:18 +0300

    First commit

--- myvisit/urls.py
+++ myvisit/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('hokliksnjkkslnknnkn/', admin.site.urls),
+    path('administrations/', admin.site.urls),
     path('', include('main.urls')),
     path('orders/', include('order.urls')),
 ]
