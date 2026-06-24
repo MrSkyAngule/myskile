0bdb886ef9f370553eeaa58167d2ae28c438cd3c
MrSkyAngule <garhok124@gmail.com>
2026-06-24 15:17:28 +0300

    First commit

--- order/models.py
+++ order/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class ServiceCart(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название услуги')
-    description = models.TextField(verbose_name='Мини описание')
-    full_description = models.TextField(verbose_name='Полное описание')
-    use_case = models.TextField('для чего используется сайт')
+    description = models.TextField(verbose_name='Мини описание', default='')
+    full_description = models.TextField(verbose_name='Полное описание', default='')
+    use_case = models.TextField(verbose_name='для чего используется сайт', default='')
     price = models.CharField(max_length=50, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
